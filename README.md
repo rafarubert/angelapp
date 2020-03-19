27502d5d96e036ed2b7b3209cb741d984f803d5c
Rafael Cruz Rubert <rafael@rubert.com.br>
2020-03-19 12:20:11 -0300

    Initial screen with buttons

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
-# AngelApp
+# angelapp
 
-## Sobre
-Aplicativo para cadastrar pessoas com impressão 3d e catalogar pessoas para solicitar máscaras para prevenção do Corona Virus.
+Aplicativo para catalogar pessoas que possam doar mascaras impressas em 3D para combater o corona virus.
 
-## Links
+## Getting Started
 
-Modelo de mascara para ser impresso:
-* [hackthepandemic](http://copper3d.com/hackthepandemic/) - Modelo de mascara para impressão
+This project is a starting point for a Flutter application.
 
-## Material Recomendando
+A few resources to get you started if this is your first Flutter project:
 
-* PETG
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/main.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Angel App',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'AngelApp - Selecione sua função'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text(widget.title),
+      ),
+      body: Center(
+
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox.fromSize(
+              size: Size(128, 128), // button width and height
+              child: ClipOval(
+                child: Material(
+                  color: Colors.blue, // button color
+                  child: InkWell(
+                    onTap: () {}, // button pressed
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(Icons.add), // icon
+                        Text(
+                          "Quero doar máscara",
+                          textAlign: TextAlign.center,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                          ),
+                        ), // text
+
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox.fromSize(
+              size: Size(128, 128), // button width and height
+              child: ClipOval(
+                child: Material(
+                  color: Colors.blue, // button color
+                  child: InkWell(
+                    onTap: () {}, // button pressed
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(Icons.favorite), // icon
+                        Text(
+                          "Preciso de uma máscara",
+                          textAlign: TextAlign.center,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ), // text
+
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
